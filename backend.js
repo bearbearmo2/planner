f7eb7a0db24bd4d91d037d392dca02a7185416f0
bearbearmo2 <mostynmw@gmail.com>
2021-02-18 23:23:14 +0000

    other day colours

--- backend.js
+++ backend.js
@@ -4,28 +4,52 @@ const days = [{
         learning: "",
         projects: "",
         exercise: "",
-        other: ""
+        other: "",
+        pallet: [
+            ["hsl(157, 16%, 28%)", "hsl(157, 16%, 70%)"],
+            ["hsl(320, 17%, 17%)", "hsl(320, 17%, 70%)"], 
+            ["hsl(148, 30%, 46%)", "hsl(148, 30%, 70%)"], 
+            ["hsl(90, 18%, 64%)", "hsl(90, 18%, 30%)"], 
+            ["hsl(78, 17%, 73%)", "hsl(78, 17%, 30%)"]]
     },
     {
         name: "Monday",
         learning: "",
         projects: "",
         exercise: "",
-        other: ""
+        other: "",
+        pallet: [
+            ["hsl(240, 69%, 3%)", "hsl(240, 69%, 70%)"], 
+            ["hsl(31, 72%, 65%)", "hsl(31, 72%, 30%)"], 
+            ["hsl(51, 58%, 82%)", "hsl(51, 58%, 30%)"], 
+            ["hsl(101, 31%, 55%)", "hsl(101, 31%, 30%)"], 
+            ["hsl(17, 64%, 52%)", "hsl(17, 64%, 80%)"]]
     },
     {
         name: "Tuesday",
         learning: "",
         projects: "",
         exercise: "",
-        other: ""
+        other: "",
+        pallet: [
+            ["hsl(203, 7%, 24%)", "hsl(203, 7%, 70%)"], 
+            ["hsl(8, 80%, 56%)", "hsl(8, 80%, 20%)"], 
+            ["hsl(65, 43%, 95%)", "hsl(65, 43%, 30%)"], 
+            ["hsl(298, 18%, 31%)", "hsl(298, 18%, 70%)"], 
+            ["hsl(59, 100%, 60%)", "hsl(59, 100%, 30%)"]]
     },
     {
         name: "Wednesday",
         learning: "",
         projects: "",
         exercise: "",
-        other: ""
+        other: "",
+        pallet: [
+            ["hsl(293, 29%, 16%)", "hsl(293, 29%, 70%)"], 
+            ["hsl(252, 41%, 34%)", "hsl(252, 41%, 70%)"], 
+            ["hsl(200, 39%, 59%)", "hsl(200, 39%, 30%)"], 
+            ["hsl(175, 58%, 85%)", "hsl(175, 58%, 30%)"], 
+            ["hsl(109, 92%, 95%)", "hsl(109, 92%, 30%)"]]
     },
     {
         name: "Thursday",
@@ -59,8 +83,13 @@ const days = [{
         projects: "",
         exercise: "",
         other: "",
-        pallet: []
+        pallet: [
+            ["hsl(215, 50%, 23%)", "hsl(215, 50%, 70%)"], 
+            ["hsl(203, 39%, 44%)", "hsl(203, 39%, 70%)"], 
+            ["hsl(182, 43%, 76%)", "hsl(182, 43%, 30%)"], 
+            ["hsl(105, 55%, 96%)", "hsl(105, 55%, 20%)"], 
+            ["hsl(355, 78%, 56%)", "hsl(355, 78%, 30%)"]]
     }
 ];
-const day = days[date.getDay()];
+let day = days[date.getDay()];
 const data = ["learning", "projects", "exercise", "other"];
